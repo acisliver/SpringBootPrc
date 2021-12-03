c315dacb348285234c0e39c924d5b45337b02abd
acisliver <7273322@naver.com>
2021-12-04 02:29:09 +0900

    캐시 삭제 확인

---
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -0,0 +1,55 @@
+package org.example.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        // given
+        String hello = "hello";
+
+        // when
+        ResultActions perform = mvc.perform(get("/hello"));
+
+        // then
+        perform
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        // when
+        ResultActions perform =
+                mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)));  // 파라미터는 문자열만 올 수 있다.
+
+        // then
+        perform
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
